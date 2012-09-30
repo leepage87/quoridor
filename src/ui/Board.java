0cd152d646276bbd979f00aac5a529c8fe443ba8
Lee Page <pagel194@potsdam.edu>
2012-09-30 18:04:47 -0400

    Added properties BoardButton, among other things.

--- src/ui/Board.java
+++ src/ui/Board.java
@@ -1,3 +1,4 @@
+package ui;
 /**
  * Tim Simmons
  * teamOrangeBeard
@@ -23,14 +24,15 @@ public class Board {
     public void setBoard(int players){
 	for(int i = 0; i < 17; i++){
 	    for(int j = 0; j < 17; j++){
-		graph[i][j] = 0;
+		grid[i][j] = 0;
 	    }
 	}
-	graph[0][8] = P1;
-	graph[16][8] = P2;
+	grid[0][8] = P1;
+
+	grid[16][8] = P2;
 	if(players == 4){
-	    graph[8][0] = P3;
-	    graph[8][16] = P4;
+	    grid[8][0] = P3;
+	    grid[8][16] = P4;
 	}
     }
 
@@ -42,27 +44,27 @@ public class Board {
 	int row = here[0];
 	int column = here[1];
 	if(direction == 'N'){ // TODO: check for edge of board
-	    if(graph[row-1][column] != WALL){ // TODO: check pawn collision
-		graph[row][column] = 0;
-		graph[row-2][column] = Player;
+	    if(grid[row-1][column] != WALL){ // TODO: check pawn collision
+		grid[row][column] = 0;
+		grid[row-2][column] = Player;
 	    }
 	}
 	if(direction == 'S'){
-	    if(graph[row+1][column] != WALL){
-		graph[row][column] = 0;
-		graph[row+2][column] = Player;
+	    if(grid[row+1][column] != WALL){
+		grid[row][column] = 0;
+		grid[row+2][column] = Player;
 	    }
 	}
 	if(direction == 'E'){
-	    if(graph[row][column+1] != WALL){
-		graph[row][column] = 0;
-		graph[row][column+2] = Player;
+	    if(grid[row][column+1] != WALL){
+		grid[row][column] = 0;
+		grid[row][column+2] = Player;
 	    }
 	}
 	if(direction == 'W'){
-	    if(graph[row][column-1] != WALL){
-		graph[row][column] = 0;
-		graph[row][column-2] = Player;
+	    if(grid[row][column-1] != WALL){
+		grid[row][column] = 0;
+		grid[row][column-2] = Player;
 	    }
 	}
     }
@@ -73,7 +75,7 @@ public class Board {
 	int[] location = new int[2];
 	for(int row = 0; row < 17; row++){
 	    for(int column = 0; column < 17; column++){
-		if(graph[i][j] == Player){
+		if(grid[row][column] == Player){
 		    location[0] = row;
 		    location[1] = column;
 		    break;
@@ -86,13 +88,13 @@ public class Board {
     // Returns: if any player is in their winning row
     public boolean winCheck(){
 	for(int i = 0; i < 17; i += 2){
-	    if(graph[16][i] == P1)
+	    if(grid[16][i] == P1)
 		return true;
-	    if(graph[0][i] == P2)
+	    if(grid[0][i] == P2)
 		return true;
-	    if(graph[i][16] == P3)
+	    if(grid[i][16] == P3)
 		return true;
-	    if(graph[i][0] == P4)
+	    if(grid[i][0] == P4)
 		return true;
 	}
 	return false;
@@ -110,7 +112,7 @@ public class Board {
 
     // Paramters: an int representing a player
     // Returns: if the player can reach their win state
-    private boolean doSearch(){
+    private boolean doSearch(int i){
 	return true;
     }
 
--- src/ui/GameBoardWithButtonsPrototype.java
+++ src/ui/GameBoardWithButtonsPrototype.java
@@ -11,10 +11,9 @@ import java.awt.event.MouseListener;
 import javax.swing.*;
 
 
-public class GameBoardWithButtonsPrototype extends JFrame implements ActionListener
+public class GameBoardWithButtonsPrototype extends JFrame /*implements ActionListener*/
 {
-	Icon defaultIcon = new ImageIcon(GameBoardWithButtonsPrototype.class.getResource("default.gif"));
-	Icon playerOne = new ImageIcon(GameBoardWithButtonsPrototype.class.getResource("player1.gif"));
+
 	
 	public GameBoardWithButtonsPrototype()  {
 		super();
@@ -27,19 +26,18 @@ public class GameBoardWithButtonsPrototype extends JFrame implements ActionListe
 		JButton[][] tokenSquares = new JButton[9][9];
 		for (int i = 0; i < 9; i++)	{
 			for (int j = 0; j < 9; j++)	{
-				tokenSquares[i][j] = new JButton();
+				tokenSquares[i][j] = new BoardButton(""+i + j);
 				tokenSquares[i][j].setBounds(SIZE*i/9 + 5, SIZE*j/9+5, 60,60);
 				//tokenSquares[i][j].setBackground(new Color(0,99,109));
-				tokenSquares[i][j].setIcon(defaultIcon);
-				tokenSquares[i][j].setPressedIcon(playerOne);
-				tokenSquares[i][j].setVisible(true);
-				tokenSquares[i][j].setRolloverEnabled(true);
-				tokenSquares[i][j].setRolloverIcon(playerOne);
-				tokenSquares[i][j].setSelectedIcon(playerOne);
-				add(tokenSquares[i][j]);
-				tokenSquares[i][j].addMouseListener(this);
+				//tokenSquares[i][j].setIcon(defaultIcon);
+				//tokenSquares[i][j].setPressedIcon(playerOne);
+
 				
-				tokenSquares[i][j].setActionCommand("Appear");
+				//tokenSquares[i][j].setSelectedIcon(playerOne);
+				add(tokenSquares[i][j]);
+				//tokenSquares[i][j].addMouseListener(this);
+				
+				//tokenSquares[i][j].setActionCommand("Appear");
 			}
 		}
 			/*tokenSquares[0][8].setIcon(player1Icon);
@@ -49,20 +47,20 @@ public class GameBoardWithButtonsPrototype extends JFrame implements ActionListe
 		JButton[][] vertwalls = new JButton[10][10];
 		for (int i = 1; i < 9; i++)	{
 			for (int j = 1; j < 10; j++)	{
-				vertwalls[i][j] = new JButton();
+				vertwalls[i][j] = new BoardWall("" + i + j + "V");
 				vertwalls[i][j].setBounds(SIZE*i/9 - 5, SIZE*(j-1)/9 + 5, 10, 60);
-				vertwalls[i][j].setBackground(Color.BLUE);
+				//vertwalls[i][j].setBackground(Color.BLUE);
 				vertwalls[i][j].setVisible(true);
 				add(vertwalls[i][j]);
 			}
 		}
 		
-		JButton[][] horzwalls = new JButton[10][10];
+		JButton[][] horzwalls = new BoardWall[10][10];
 		for (int i = 1; i < 9; i++) {
 			for (int j = 1; j < 10; j++) {
-				horzwalls[i][j] = new JButton();
+				horzwalls[i][j] = new BoardWall("" + i + j + "H");
 				horzwalls[i][j].setBounds(SIZE*(j-1)/9 + 5, SIZE*i/9 - 5, 60, 10);
-				horzwalls[i][j].setBackground(Color.ORANGE);
+				//horzwalls[i][j].setBackground(Color.ORANGE);
 				horzwalls[i][j].setVisible(true);
 				add(horzwalls[i][j]);
 			}
@@ -95,13 +93,14 @@ public class GameBoardWithButtonsPrototype extends JFrame implements ActionListe
 			 */
 		}
 	}
+
 	
-	
+	/*
 	public void mouseClicked(MouseEvent e) {
 	       e.getComponent().setIcon(playerOne);
 	       }
-	
-	
+	*/
+	/*
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if ("Appear".equals(e.getActionCommand())) {
@@ -109,4 +108,5 @@ public class GameBoardWithButtonsPrototype extends JFrame implements ActionListe
 		}
 		
 	}
+	*/
 }
