70a374ab9894d63f3c371e790f9b8c9b212a375d
Lee Page <pagel194@potsdam.edu>
2012-10-08 19:05:20 -0400

    Added png files (images for tokens) and GameBoardTest. -jg

---
+++ test/gui_test/GameBoardTest.java
@@ -0,0 +1,116 @@
+package test.gui_test;
+
+import static src.ui.GameBoardWithButtonsPrototype.*;
+
+import javax.swing.JButton;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.text.IsEqualIgnoringWhiteSpace.equalToIgnoringWhiteSpace;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import src.ui.FirstWindow;
+import src.ui.GameBoardWithButtonsPrototype;
+
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.ComponentDriver;
+import com.objogate.wl.swing.driver.JButtonDriver;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+ 
+public class GameBoardTest {
+	
+	
+	
+	JFrameDriver driver;
+	@SuppressWarnings("unchecked")
+	@Before
+	public void setUp() throws Exception {
+		GameBoardWithButtonsPrototype.main();
+		driver = new JFrameDriver(new GesturePerformer(), new AWTEventQueueProber(), 
+				JFrameDriver.named(MAIN_WINDOW_TITLE), JFrameDriver.showingOnScreen());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.dispose();
+	}
+
+    @SuppressWarnings("unchecked")
+    private JButtonDriver button(String name) {
+        return new JButtonDriver(driver, JButton.class, ComponentDriver.named(name));
+    }
+
+    @SuppressWarnings("unchecked")
+    private JLabelDriver label(String name) {
+        return new JLabelDriver(driver, ComponentDriver.named(name));
+    }
+	
+	
+	@Test
+	public void windowUpWithTitle() {
+		driver.hasTitle(MAIN_WINDOW_TITLE);
+	}
+	/*
+	@Test
+	public void windowContainsButtons() {
+		for (int i = 0; i < 9; ++i) {
+			for (int j = 0; j < 9; j++)	{
+			String buttonName = "Button"+ i + "" +j;
+			JButtonDriver button = button(buttonName);
+			button.click();
+			}
+		}
+	}
+	*/
+	/*@Test
+	public void windowContainsVerticalWalls() {
+		for (int i = 1; i < 9; i++)	{
+			for (int j = 1 + i%2; j < 9; j+=2)	{
+				String wallName = "VertWall" + i + "" + j;
+				JButtonDriver button = button(wallName);
+				button.click();
+			}
+		}
+	}*/
+	
+	@Test
+	public void windowContainsHorizontalWalls() {
+		for (int i = 1; i < 9; i++) {
+			for (int j = 1 + i%2; j < 9; j+=2) {
+				String wallName = "HorzWall" + i + "" + j;
+				JButtonDriver button = button(wallName);
+				button.click();
+			}
+		}
+	}
+	
+}
+	/*
+	@Test
+	public void labelChangesWithButtonPushes() {
+		JLabelDriver label = label(LABEL_NAME);
+		
+		label.hasText(equalTo(INITIAL_MESSAGE));
+		for (int i = 0; i < BUTTON_TEXTS.length; ++i) {
+			String buttonName = String.format("%s%d", BUTTON_NAME_PREFIX, i);
+			JButtonDriver button = button(buttonName);
+			button.click();
+			label.hasText(equalTo(MESSAGES[i]));
+		}
+
+		// Make sure they can go back to previous values
+		for (int i = 0; i < BUTTON_TEXTS.length; ++i) {
+			String buttonName = String.format("%s%d", BUTTON_NAME_PREFIX, i);
+			JButtonDriver button = button(buttonName);
+			button.click();
+			label.hasText(equalTo(MESSAGES[i]));
+		}
+	}	
+*/
+
