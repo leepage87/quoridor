07a4a0cb8f915ac855cf92381b5fe223d2a5c99e
Sarah Weller <wellersi192@dix.(none)>
2012-10-18 14:46:04 -0400

    AI

--- src/ui/AI.java
+++ src/ui/AI.java
@@ -1,49 +1,46 @@
 package src.ui;
 import src.ui.Board;
+//import src.ui.PlayQuor;
 
 public class AI{
 
 	
 	   final int NUMPLAY;
 	   final int WALL = 5;
-	   int[][] grid;
+	   int[][] grid = new int[17][17];
 	   Board x = new Board(2);
-
-		 
-	// Parameters: the number of players (2 or 4)
-	// Creates: a board object
-	public AI(int players){
-		 this.grid = new int[17][17];
-		 	setBoard(players);
-		        if(players == 4)
-		            NUMPLAY = 4;
-		        else
-		            NUMPLAY = 2;
-		    }
+	   int[] move = new int[4];
 
   // Parameters: a board
  // Creates: a board object that is a duplicate of the board given
-	public AI(AI b){
+	public AI(Board b, int player, int numOfPlayers){
 		  this.grid = b.grid;
 	      this.NUMPLAY = b.NUMPLAY;
+	      currentLoc(numOfPlayers);
+	      move(player);
 		    }
-
-		    // Parameters: the number of players (2 or 4)
-		    // PostCondition: the board is empty and two or four players exist
-	public void setBoard(int players){
-		  for(int i = 0; i < 17; i++){
-		      for(int j = 0; j < 17; j++){
-		          grid[i][j] = 0;
-		       }
-		   }
-	    }
 	
-	public void main(String[]args){
-		int i = 1;
-		x.playerPlace(i);
+	public void currentLoc(int numOfPlayers){
+		if(numOfPlayers==2){
+			x.playerPlace(1);
+			x.playerPlace(2);
+		}else if(numOfPlayers==4){
+			x.playerPlace(1);
+			x.playerPlace(2);
+			x.playerPlace(3);
+			x.playerPlace(4);
+		}
+	}
+	public void move(int playerTurn){
+		while(playerTurn == 1){
+			move = x.bestMove(1);
+		}for(int i = 4; i<=4; i++){
+			grid[move[3]][move[4]]=grid[move[0]][move[1]];
+			grid[move[0]][move[1]]= 0;
+		}
 		
-	}
-	}
+	}	
+}	
 
 	
 
